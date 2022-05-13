4ac3bfea47d5878d93f0a72f04d9b40554bb7261
Takanori Ishikawa <ishikawa_takanori@mac.com>
2022-05-13 13:46:10 +0900

    add lib

---
+++ example/src/App.tsx
@@ -0,0 +1,42 @@
+import * as React from 'react';
+
+import {StyleSheet, View, Text} from 'react-native';
+import {
+  multiply,
+  CBUUIDCharacteristicUserDescriptionString,
+  Constant1,
+  Constant2,
+} from 'react-native-core-bluetooth';
+
+export default function App() {
+  const [result, setResult] = React.useState<number | undefined>();
+
+  React.useEffect(() => {
+    multiply(3, 7).then(setResult);
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Text>Result: {result}</Text>
+      <Text>
+        CBUUIDCharacteristicUserDescriptionString:{' '}
+        {CBUUIDCharacteristicUserDescriptionString}
+      </Text>
+      <Text>Constant1: {Constant1}</Text>
+      <Text>Constant2: {Constant2}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  box: {
+    width: 60,
+    height: 60,
+    marginVertical: 20,
+  },
+});
